6e72c649141eafd9f88510d12d7d4883e0dde112
Sumant Shah <sumant.shah@gigacars.com>
2024-04-08 17:09:11 +0530

    post request task

--- src/routers/task.js
+++ src/routers/task.js
@@ -1,7 +1,21 @@
 const express = require('express')
 const Task = require('../models/task')
-
+const auth = require('../middleware/auth')
 const router = new express.Router()
+
+router.post('/tasks', auth, async(req, res) => {
+    const task = new Task({
+        ...req.body,
+        owner: req.user._id
+    })
+
+    try{
+        await task.save()
+        res.status(201).send(task)
+    }catch (err) {
+        res.status(400).send(e)
+    }
+})
 
 
 module.exports = router

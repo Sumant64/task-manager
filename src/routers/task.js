2880b3afdef9c46c7ee88efe6d5a392111823bd6
Sumant Shah <sumant.shah@gigacars.com>
2024-04-09 07:11:50 +0530

    get all tasks

--- src/routers/task.js
+++ src/routers/task.js
@@ -4,6 +4,7 @@ const auth = require('../middleware/auth')
 const router = new express.Router()
 
 router.post('/tasks', auth, async(req, res) => {
+    console.log('hit')
     const task = new Task({
         ...req.body,
         owner: req.user._id
@@ -14,6 +15,20 @@ router.post('/tasks', auth, async(req, res) => {
         res.status(201).send(task)
     }catch (err) {
         res.status(400).send(e)
+    }
+})
+
+router.get('/tasks', auth, async(req, res) => {
+    try{
+        //first method
+        // await req.user.populate('tasks')
+        // res.send(req.user.tasks)
+
+        // second method
+        const task = await Task.find({owner: req.user._id}).populate('owner')
+        res.send(task)
+    } catch (err) {
+        res.status(500).send(err)
     }
 })
 

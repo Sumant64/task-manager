aef5f281a5145415e402f0e3b04e9bb3a843fd7a
Sumant Shah <sumant.shah@gigacars.com>
2024-04-09 08:20:50 +0530

    delete user

--- src/routers/user.js
+++ src/routers/user.js
@@ -74,4 +74,14 @@ router.patch('/users/me', auth, async (req, res) => {
     }
 })
 
+
+router.delete('/users/me', auth, async (req, res) => {
+    try{
+        await req.user.remove()
+        res.send(req.user)
+    } catch (e) {
+        res.status(500).send()
+    }
+})
+
 module.exports = router

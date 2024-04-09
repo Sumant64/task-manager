fe453a34ad37d3e32b5f4f0541441c503a1324e5
Sumant Shah <sumant.shah@gigacars.com>
2024-04-09 07:35:00 +0530

    patch task by id

--- src/routers/task.js
+++ src/routers/task.js
@@ -47,5 +47,29 @@ router.get('/tasks/:id', auth, async (req, res) => {
     }
 })
 
+router.patch('/tasks/:id', auth, async (req, res) => {
+    const updates = Object.keys(req.body)
+    const allowedUpdates = ['description', 'complete']
+    const isValidOperation = updates.every((update) => allowedUpdates.includes(update))
+
+    if(!isValidOperation) {
+        return res.status(400).send({ error: 'Invalid updates! '})
+    }
+
+    try{
+        const task = await Task.findOne({ _id: req.params.id, owner: req.user._id })
+        
+        if(!task) {
+            return res.status(400).send()
+        }
+
+        updates.forEach((update) => task[update] = req.body[update])
+        await task.save()
+        res.send(task)
+    } catch(e) {
+        res.status(400).send(e)
+    }
+})
+
 
 module.exports = router
